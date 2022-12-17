f20f49fdef2af57af8398ef6b73702d5c849dbe6
flores-o <oana.florescu28@gmail.com>
2022-12-16 23:15:12 -0800

    fix merge conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -75,10 +75,10 @@ const App = () => {
     // console.log("genereated reply: ", generated_reply)
 
     // ========== You win or lose? =================
-    const endCase = await categorizeGeneratedObject(prompt)
-    // console.log("endCase: ", endCase)
-    setGameResults(getGameResult(endCase))
-    // console.log("updated gameResults: ", gameResults.gameEnded)
+    const endCase = await categorizeGeneratedObject(prompt);
+    console.log("endCase: ", endCase)
+    setGameResults(getGameResult(endCase));
+    console.log("updated gameResults: ", gameResults.gameEnded)
     // ============================================
 
     setLoading(false)
@@ -96,8 +96,9 @@ const App = () => {
     CatFliesDown: 1,
     CatFliesUp: 0,
     TreeTakesFire: 0,
-    TreeCutDown: 1,
-    Other: -1,
+    TreeCutDown: 1, 
+    CatGoesToHeaven: 0,
+    Other: -1
   }
   async function isGPTResponseAffirmative(response) {
     // response: string
@@ -117,25 +118,75 @@ const App = () => {
   async function categorizeGeneratedObject(prompt) {
     // prompt: string
     // return: EndCase
-    const question1HumanLiftsP = `Can a human easily lift a ${prompt}? Only answer with yes or no.` // if yes, don't do anythin
-    const question2PCutsDownTree = `Is a ${prompt} sharp enough to cut down a tree? Only answer with yes or no.`
-    const question3PStartsFire = `Can a ${prompt} be used to start a fire directly? Only answer with yes or no.`
+    const question1HumanLiftsP = `Can a human easily lift a ${prompt}? Only answer with yes or no.`; 
+    const question2PCutsDownTree = `Is a ${prompt} sharp enough to cut down a tree? Only answer with yes or no.`; 
+    const question3PStartsFire = `Can a ${prompt} be used to start a fire directly? Only answer with yes or no.`;
+    const question4CatLikesP = `Do cats like ${prompt}? Only answer with yes or no.`
+    const question5PUsedToFly = `Can ${prompt} be used to fly? Only answer with yes or no.`
+    const question6PLivingCreature = `Is ${prompt} a living creature? Only answer with yes or no.`
+    const question7PCausesHarmToCat = `Is it likely that a ${prompt} will cause harm to a cat if locked in a room together? Only answer with yes or no.`
+    const question8PSavesCatsFromTrees = `Do ${prompt} save cats from trees often? Only answer with yes or no`
 
     const gptResponse1CutsDownTree = await askGpt(question1HumanLiftsP)
     console.log("gptResponse1CutsDownTree: ", gptResponse1CutsDownTree)
-    const gptResponse1CutsDownTreeBoolean = await isGPTResponseAffirmative(gptResponse1CutsDownTree)
-    console.log("gptResponse1CutsDownTreeBoolean: ", gptResponse1CutsDownTreeBoolean)
-    const gptResponse2PCutsDownTree = await askGpt(question2PCutsDownTree)
+    const gptResponse1CutsDownTreeBoolean = await isGPTResponseAffirmative(gptResponse1CutsDownTree);
+    console.log("gptResponse1CutsDownTreeBoolean: ", gptResponse1CutsDownTreeBoolean);
+
+    const gptResponse2PCutsDownTree = await askGpt(question2PCutsDownTree);
     console.log("gptResponse2PCutsDownTree: ", gptResponse2PCutsDownTree)
-    const gptResponse2PCutsDownTreeBoolean = await isGPTResponseAffirmative(gptResponse2PCutsDownTree)
-    console.log("gptResponse2PCutsDownTreeBoolean: ", gptResponse2PCutsDownTreeBoolean)
-    const gptResponse3PStartsFire = await askGpt(question3PStartsFire)
+    const gptResponse2PCutsDownTreeBoolean = await isGPTResponseAffirmative(gptResponse2PCutsDownTree);
+    console.log("gptResponse2PCutsDownTreeBoolean: ", gptResponse2PCutsDownTreeBoolean);
+
+    const gptResponse3PStartsFire = await askGpt(question3PStartsFire);
     console.log("gptResponse3PStartsFire : ", gptResponse3PStartsFire)
     const gptResponse3PStartsFireBoolean = await isGPTResponseAffirmative(gptResponse3PStartsFire)
     console.log("gptResponse3PStartsFire Boolean: ", gptResponse3PStartsFireBoolean)
 
-    if (gptResponse1CutsDownTreeBoolean & gptResponse2PCutsDownTreeBoolean) {
-      return EndCase.TreeCutDown
+    const gptResponse4CatLikesP = await askGpt(question4CatLikesP);
+    console.log("gptResponse4CatLikesP : ", gptResponse4CatLikesP)
+    const gptResponse4CatLikesPBoolean = await isGPTResponseAffirmative(gptResponse4CatLikesP);
+    console.log("gptResponse4CatLikesP Boolean: ", gptResponse4CatLikesPBoolean);
+
+    const gptResponse5PUsedToFly = await askGpt(question5PUsedToFly);
+    console.log("gptResponse5PUsedToFly : ", gptResponse5PUsedToFly)
+    const gptResponse5PUsedToFlyBoolean = await isGPTResponseAffirmative(gptResponse5PUsedToFly);
+    console.log("gptResponse5PUsedToFly Boolean: ", gptResponse5PUsedToFlyBoolean);
+
+    const gptResponse6PLivingCreature = await askGpt(question6PLivingCreature);
+    console.log("gptResponse6PLivingCreature : ", gptResponse6PLivingCreature)
+    const gptResponse6PLivingCreatureBoolean = await isGPTResponseAffirmative(gptResponse6PLivingCreature);
+    console.log("gptResponse6PLivingCreature Boolean: ", gptResponse6PLivingCreatureBoolean);
+
+    const gptResponse7PCausesHarmToCat = await askGpt(question7PCausesHarmToCat);
+    console.log("gptResponse7PCausesHarmToCat : ", gptResponse7PCausesHarmToCat)
+    const gptResponse7PCausesHarmToCatBoolean = await isGPTResponseAffirmative(gptResponse7PCausesHarmToCat);
+    console.log("gptResponse7PCausesHarmToCat Boolean: ", gptResponse7PCausesHarmToCatBoolean);
+
+    const gptResponse8PSavesCatsFromTrees = await askGpt(question8PSavesCatsFromTrees);
+    console.log("gptResponse8PSavesCatsFromTrees : ", gptResponse8PSavesCatsFromTrees)
+    const gptResponse8PSavesCatsFromTreesBoolean = await isGPTResponseAffirmative(gptResponse8PSavesCatsFromTrees);
+    console.log("gptResponse8PSavesCatsFromTreesBoolean: ", gptResponse8PSavesCatsFromTreesBoolean);
+
+    if (gptResponse1CutsDownTreeBoolean & gptResponse2PCutsDownTreeBoolean){
+      return EndCase.TreeCutDown;
+    }
+    if (gptResponse3PStartsFireBoolean){
+      return EndCase.TreeTakesFire;
+    }
+    if (gptResponse4CatLikesPBoolean){
+      return EndCase.CatGoesDown;
+    }
+
+    if (gptResponse5PUsedToFlyBoolean){
+      return EndCase.CatFliesUp; // todo: return randomly between CatFliesDown and CatFliesUp
+    }
+
+    if (gptResponse6PLivingCreatureBoolean & gptResponse7PCausesHarmToCatBoolean){
+      return EndCase.CatGoesToHeaven;
+    }
+
+    if (gptResponse6PLivingCreature & gptResponse8PSavesCatsFromTreesBoolean){
+      return EndCase.CatGoesDown;
     }
 
     return EndCase.Other
